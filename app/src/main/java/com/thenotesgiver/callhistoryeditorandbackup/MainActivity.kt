e85201ae02e89ac5dbb869100bc6c217c2ced599
Manjeet Deswal <69675268+manjeetdeswal@users.noreply.github.com>
2025-06-02 07:40:13 +0530

    Update MainActivity.kt

--- app/src/main/java/com/thenotesgiver/callhistoryeditorandbackup/MainActivity.kt
+++ app/src/main/java/com/thenotesgiver/callhistoryeditorandbackup/MainActivity.kt
@@ -1,24 +1,3 @@
-/*
- * SMS Import / Export: a simple Android app for importing and exporting SMS and MMS messages,
- * call logs, and contacts, from and to JSON / NDJSON files.
- *
- * Copyright (c) 2021-2023 Thomas More
- *
- * This file is part of SMS Import / Export.
- *
- * SMS Import / Export is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SMS Import / Export is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SMS Import / Export.  If not, see <https://www.gnu.org/licenses/>
- */
 
 package com.thenotesgiver.callhistoryeditorandbackup
 
